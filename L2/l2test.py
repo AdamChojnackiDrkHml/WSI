d2ec796a983e701969e9bca89f8ec52b71c18c37
AdamChojnackiDrkHml <72651971+AdamChojnackiDrkHml@users.noreply.github.com>
2021-11-19 10:22:42 +0100

    a

--- L2/l2test.py
+++ L2/l2test.py
@@ -9,10 +9,6 @@ mnist = tf.keras.datasets.mnist
 images_filename = 'my-images.idx3-ubyte'
 label_filename = 'my-labels.idx1-ubyte'
 
-(training_data, training_labels), (test_data, test_labels) = mnist.load_data()
-test_data = test_data / 255
-
-
 train_images, train_labels = loadlocal_mnist(
             images_path=images_filename, 
             labels_path=label_filename)
