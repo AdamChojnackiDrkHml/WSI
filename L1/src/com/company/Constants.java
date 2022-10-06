29b55e02edb705f3b0056d336c4041d60fbeb79f
AdamChojnackiDrkHml <72651971+AdamChojnackiDrkHml@users.noreply.github.com>
2022-10-06 23:05:11 +0200

    Delete unwated files

--- L1/src/com/company/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public final class Constants
-{
-    public static final int BOARD_SIZE = 4;
-    public static final HeuristicEnum CURR_HEURISTIC = HeuristicEnum.MANHATTAN;
-}
